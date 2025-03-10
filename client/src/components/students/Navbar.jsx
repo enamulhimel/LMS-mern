342b2667c573853023d3f99ded6de62f7f4a0538
bdCalling <bdcalling@bdCallings-Mac-mini.local>
2025-03-10 12:35:41 +0600

    home page complete

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -3,10 +3,12 @@ import { assets } from "./../../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { AppContext } from "../../context/AppContext";
+import Educator from '../../pages/educator/Educator';
 
 const Navbar = () => {
   const navigate = useNavigate();
   // const navigate = useContext(AppContext)
+  const {isEducator,setIsEducator} = useContext(AppContext)
   const isCourseListPage = location.pathname.includes("/course-list");
 
   const { openSignIn } = useClerk();
@@ -31,7 +33,7 @@ const Navbar = () => {
         <div className="flex items-center gap-5">
           {user && (
             <>
-              <button>Become Educator</button>
+              <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
               {"|"}
               <Link to={"/my-enrollment"}>My Enrollment</Link>
             </>
@@ -52,7 +54,7 @@ const Navbar = () => {
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
           {user && (
             <>
-              <button>Become Educator</button>
+              <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
               {"|"}
               <Link to={"/my-enrollment"}>My Enrollment</Link>
             </>
