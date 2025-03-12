c4254aeb982b694786005e2aab97f34461b3e69f
bdCalling <bdcalling@bdCallings-Mac-mini.local>
2025-03-12 15:53:53 +0600

    course details page updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import {dummyCourses} from "../assets/assets"
 import { useNavigate } from "react-router-dom";
+import humanize from 'humanize-duration'
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -28,11 +30,38 @@ export const AppContextProvider = ({ children }) => {
     return totalRating / course.courseRatings.length
   }
 
+  //Function to calculate course chapter time
+  const calculateChapterTime = (chapter)=>{
+    let time = 0;
+    chapter.chapterContent.map((lecture)=>time += lecture.lectureDuration)
+    return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+  }
+
+  //Function to calculate course duration time
+  const calculateCourseTime = (chapter) =>{
+    let time = 0 ;
+    course.chapterContent.map((chapter)=>chapter.chapterContent.map((lecture)=>{
+      time += lecture.lectureDuration
+    }))
+    return humanizeDuration(time *60*1000,{units:["h","m"]})
+  }
+
+  //Function to calculate no. of lechtures in the course
+  const calculateNoOfLectures = (course)=>{
+    const totalLectures = 0;
+    course.courseContent.forEach(chapter=>{
+      if(Array.isArray(chapter.chapterContent)){
+        totalLectures += chapter.chapterContent.length;
+      }
+    })
+    return totalLectures;
+  }
+
 
   useEffect(()=>{
     fetchAllCourses()
   },[])
 
-  const value = {currency,allCourses,navigate,calculateRating,isEducator,setIsEducator};
+  const value = {currency,allCourses,navigate,calculateRating,isEducator,setIsEducator,calculateNoOfLectures,calculateCourseTime,calculateChapterTime};
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
